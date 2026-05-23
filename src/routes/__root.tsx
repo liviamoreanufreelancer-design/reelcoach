a2aa4dd0e0d5b4b9aff41c5861e7eddbfb5df063
liviamoreanufreelancer-design <livia.moreanu.freelancer@gmail.com>
2026-05-23 19:11:10 +0300

    fonts

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -90,7 +90,7 @@ export const Route = createRootRouteWithContext<{ queryClient: QueryClient }>()(
       { rel: "preconnect", href: "https://fonts.gstatic.com", crossOrigin: "anonymous" },
       {
         rel: "stylesheet",
-        href: "https://fonts.googleapis.com/css2?family=Instrument+Serif:ital@0;1&family=Fraunces:ital,opsz,wght@0,9..144,300..700;1,9..144,300..700&family=Geist:wght@300;400;500;600;700;800&display=swap",
+        href: "https://fonts.googleapis.com/css2?family=Instrument+Serif:ital@0;1&display=swap",
       },
     ],
   }),
